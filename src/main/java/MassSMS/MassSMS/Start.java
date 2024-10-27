47bd79766ce53ad4632acf3f9910e3e676b2b4a8
krummy0 <jlkrumholz01@gmail.com>
2024-10-27 14:26:28 -0600

    Create Start.java
    Add Main

---
+++ src/main/java/MassSMS/MassSMS/Start.java
@@ -0,0 +1,38 @@
+package MassSMS.MassSMS;
+
+//TODO Multithread
+//TODO Work on UI
+//TODO Save and load settings
+//TODO Settings not loading from variables into gui
+//TODO Test Send and Test
+//TODO Fix from
+//TODO remove api dependancy for speed and usage costs
+
+public class Start {
+
+	public static void main(String[] args) {
+		gui ui = new gui();
+		
+		
+//		String key = "";
+//	
+//		String[] nums = {""};
+//		SMS sms = new SMS(nums, key, true);
+//		
+//		
+//		// Replace with your SMTP server details and credentials
+//		String stmpHost = ""; // SMTP server (e.g., stmp.gmail.com)
+//		int stmpPort = 587; // Port number (587 for TLS, 465 for SSL, 25 for any)
+//		String username = ""; // Your email
+//		String password = ""; // Your password
+//		String encryptionType = "TLS"; // Set to "SSL", "TLS", or leave empty for no encryption
+//
+//		try {
+//			STMP emailSender = new STMP(stmpHost, stmpPort, username, password, encryptionType);
+//			emailSender.sendEmail("3039295058", sms.getAdresses()[0], "", "This is a test email body.");
+//		} catch (IllegalArgumentException e) {
+//			System.err.println(e.getMessage());
+//		}
+	}
+
+}
